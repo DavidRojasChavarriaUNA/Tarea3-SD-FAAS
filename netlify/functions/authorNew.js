65d0d891cc47d59dd0b125ff58d796c9b6a94537
David Rojas Chavarria <davidmfrojas@gmail.com>
2023-10-02 11:49:53 -0600

    cambio de código para duplicados
    cambio de código para duplicados

--- netlify/functions/authorNew.js
+++ netlify/functions/authorNew.js
@@ -21,7 +21,7 @@ exports.handler = async (event, context) => {
 
     if(client.db(dbName).collection(collection.Authors).find({"_id":data._id}).limit(1).length === 1){
       return {
-        statusCode: 422,
+        statusCode: 409,
         headers,
         body: "A record already exists with the indicated id"
       };
